002af75250d88128b9f2ce186c2b21fb95716152
rufeng-h <chunfengh163@163.com>
2022-09-20 10:02:12 +0800

    首次提交

---
+++ src/router/routes/index.ts
@@ -0,0 +1,76 @@
+import type { AppRouteRecordRaw, AppRouteModule } from '/@/router/types';
+
+import { PAGE_NOT_FOUND_ROUTE, REDIRECT_ROUTE } from '/@/router/routes/basic';
+
+import { mainOutRoutes } from './mainOut';
+import { PageEnum } from '/@/enums/pageEnum';
+import { t } from '/@/hooks/web/useI18n';
+import { LAYOUT } from '../constant';
+
+// import.meta.globEager() 直接引入所有的模块 Vite 独有的功能
+const modules = import.meta.globEager('./modules/**/*.ts');
+const routeModuleList: AppRouteModule[] = [];
+
+// 加入到路由集合中
+Object.keys(modules).forEach((key) => {
+  const mod = modules[key].default || {};
+  const modList = Array.isArray(mod) ? [...mod] : [mod];
+  routeModuleList.push(...modList);
+});
+
+export const asyncRoutes = [PAGE_NOT_FOUND_ROUTE, ...routeModuleList];
+
+// 根路由
+export const RootRoute: AppRouteRecordRaw = {
+  path: '/',
+  name: 'Root',
+  redirect: PageEnum.INDEX_HOME,
+  meta: {
+    title: 'Root',
+  },
+};
+
+export const LoginRoute: AppRouteRecordRaw = {
+  path: '/login',
+  name: 'Login',
+  component: () => import('/@/views/sys/login/Login.vue'),
+  meta: {
+    title: t('routes.basic.login'),
+  },
+};
+
+export const indexRoutes: AppRouteRecordRaw[] = [
+  {
+    path: '/index',
+    name: 'Index',
+    redirect: '/index/home',
+    meta: {
+      title: '首页',
+      hideChildrenInMenu: true,
+      ignoreAuth: true,
+    },
+    component: LAYOUT,
+    children: [
+      {
+        path: 'home',
+        name: 'IndexHome',
+        meta: {
+          title: '首页',
+          ignoreAuth: true,
+        },
+        component: () => import('/@/views/vhr/index/Home.vue'),
+      },
+    ],
+  },
+];
+
+// Basic routing without permission
+// 未经许可的基本路由
+export const basicRoutes = [
+  LoginRoute,
+  RootRoute,
+  ...mainOutRoutes,
+  REDIRECT_ROUTE,
+  PAGE_NOT_FOUND_ROUTE,
+  ...indexRoutes,
+];
---
+++ src/views/vhr/index/Home.vue
@@ -0,0 +1,1 @@
+<template><div>这是首页</div></template>
